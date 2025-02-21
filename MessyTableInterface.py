63a8463307c0771209aea5647d91e238c996f6e9
mconway2579 <maxlconway@gmail.com>
2025-02-21 14:52:37 -0700

    fix table interface

--- MessyTableInterface.py
+++ MessyTableInterface.py
@@ -13,12 +13,13 @@ import random
 import numpy as np
 
 class MessyTableDataset(Dataset):
-    def __init__(self, file_path, set_size=2, min_pixel_area=2000, img_shape = (224, 224), train=False):
-        self.image_shape = img_shape
+    def __init__(self, file_path, set_size=2, min_pixel_area=2000, train=False):
+        self.image_shapes = [128, 256, 512, 1024, 2048]
+        self.max_image_shape = (max(self.image_shapes), max(self.image_shapes))
         if train:
             self.transform = transforms.Compose([
                 transforms.Lambda(lambda img: Image.fromarray(img)),
-                transforms.Resize(self.image_shape),
+                transforms.Lambda(lambda img: transforms.Resize(self.get_img_size())(img)),
                 transforms.RandomHorizontalFlip(),
                 transforms.RandomVerticalFlip(),
                 transforms.RandomRotation(180),
@@ -27,7 +28,7 @@ class MessyTableDataset(Dataset):
         else:
             self.transform = transforms.Compose([
                 transforms.Lambda(lambda img: Image.fromarray(img)),
-                transforms.Resize(self.image_shape),
+                transforms.Resize(self.max_image_shape),
                 transforms.ToTensor()
             ])
         self.min_pixel_area = min_pixel_area
@@ -49,6 +50,8 @@ class MessyTableDataset(Dataset):
             print(f"loading csv from {csv_path}")
             self.instance_df = pd.read_csv(csv_path)
             return
+        else:
+            print(f"csv {csv_path} does not exist")
         
         #If csv file does not exist create it
         print(f"generating csv at {csv_path}")
@@ -88,42 +91,14 @@ class MessyTableDataset(Dataset):
             for entry in instance_entries.values():
                 self.instance_df = pd.concat([self.instance_df, pd.DataFrame([entry])], ignore_index=True)
         self.instance_df.to_csv(csv_path, index=False)
-
+    def get_img_size(self):
+        sidelength = random.choice(self.image_shapes)
+        #print(f"{sidelength=}")
+        return (sidelength, sidelength)
         
         # Load and display the image with bounding box
-    def debug(self):
-        images, bboxes, object_imgs, projection_matricies, instance_scene = self.__getitem__(random.randint(0, len(self.instance_df)-1))
-        fig, axes = plt.subplots(nrows=1, ncols=self.set_size, figsize=(20, 20))
-        for i, (img, bbox, object_img) in enumerate(zip(images, bboxes, object_imgs)):
-            
-            #print(f"{bbox=}")
-
-            image = img.permute(1, 2, 0)
-            object_img = object_img.permute(1, 2, 0)
-            #print(f"{image.shape=}")
-            #print(f"{object_img.shape=}")
-            new_img = torch.zeros((image.shape[0] + object_img.shape[0] + 1, max(image.shape[1], object_img.shape[1]) + 1, 3))
-            new_img[:image.shape[0], :image.shape[1], :] = image
-            new_img[image.shape[0]:image.shape[0] + object_img.shape[0], :object_img.shape[1], :] = object_img
-
-            x1, y1, x2, y2 = bbox
-            bb_color = torch.tensor([1.0, 0.0, 0.0])
-            new_img[y1, x1:x2, :] = bb_color
-            new_img[y2, x1:x2, :] = bb_color
-            new_img[y1:y2, x1, :] = bb_color
-            new_img[y1:y2, x2, :] = bb_color
-
-
-            axes[i].imshow(new_img)
-            axes[i].axis('off')
-
-        fig.suptitle(instance_scene)
-        plt.show(block = False)
-        plt.pause(1)
-
     def __len__(self):
         return len(self.instance_df)
-
     def is_valid_bbox(self, bbox):
         if type(bbox) == str:
             bbox = [float(item.strip()) for item in bbox.strip('[]').split(',')]
@@ -147,11 +122,9 @@ class MessyTableDataset(Dataset):
             return self.__getitem__(random.randint(0, len(self.instance_df)-1))
         #print(f"{non_nan_cameras=}")
         cameras_to_use = random.sample(valid_cameras, self.set_size)
-        #print(f"{cameras_to_use=}")
-        images = []
-        bboxes = []
+        
         object_imgs = []
-        projection_matricies= []
+        
         instance_scene = [f"instance:{row['instance']}_Scene:{row['scene']}" for camera in cameras_to_use]
 
         for camera in cameras_to_use:
@@ -165,98 +138,64 @@ class MessyTableDataset(Dataset):
             original_bbox = [int(np.ceil(x)) for x in original_bbox]
 
             x1, y1, x2, y2 = original_bbox
-            cropped_image = image[y1:y2, x1:x2].copy()
+            cropped_image = image[y1:y2, x1:x2]
 
-            
-
-
-            transformed_image = self.transform(image.copy())
-            images.append(transformed_image)
-
-            original_height, original_width = image.shape[:2]
-            scale_x = self.image_shape[1] / original_width
-            scale_y = self.image_shape[0] / original_height
-            scaled_bbox = [int(original_bbox[0] * scale_x), int(original_bbox[1] * scale_y), int(original_bbox[2] * scale_x), int(original_bbox[3] * scale_y)]
-            torch_bbox = torch.tensor(scaled_bbox)
-            bboxes.append(torch_bbox)
-
-            transformed_obj_image = self.transform(cropped_image.copy())
+            transformed_obj_image = self.transform(cropped_image)
             object_imgs.append(transformed_obj_image)
 
+        return object_imgs, instance_scene
 
-            
-            E = row[f'camera_{camera}_extrinsics']
-            if type(E) == str:
-                E = [float(item.strip()) for item in E.strip('[]').split()]
-            x, y, z, roll, pitch, yaw = E
-            R_x = np.array([[1, 0, 0],
-                            [0, np.cos(roll), -np.sin(roll)],
-                            [0, np.sin(roll), np.cos(roll)]])
-            
-            R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-                            [0, 1, 0],
-                            [-np.sin(pitch), 0, np.cos(pitch)]])
-            
-            R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-                            [np.sin(yaw), np.cos(yaw), 0],
-                            [0, 0, 1]])
-            
-            R = np.dot(R_z, np.dot(R_y, R_x))
-            T = np.array([x, y, z]).reshape(3, 1)
-            E = np.hstack((R, T))
-            #print(f"{E=}")
-            I = self.intrinics[camera]
-            #print(f"{I=}")
-            P = np.dot(I, E)
-            projection_matricies.append(P)
-        return images, bboxes, object_imgs, projection_matricies, instance_scene
-        
+def custom_collate(batch):
+    # print(f"{len(batch)=}")
+    # print(f"{len(batch[0])=}")
+    # print(f"{len(batch[0][0])=}")
+    # print(f"{batch[0][0][0].shape=}")
+    # print(f"{batch[0][0][1].shape=}")
+    # print(f"{batch[0][0][2].shape=}")
+
+    # print(f"{batch[0][1]=}")
+    # print()
+
+
+    object_imgs = [item[0] for item in batch]
+    instance_scene = [item[1] for item in batch]
+    return object_imgs, instance_scene
+
+
 def display_batch(data_loader):
     fig, axes = plt.subplots(ncols = data_loader.batch_size, nrows = data_loader.dataset.set_size)
-    data = next(iter(data_loader))
-    images_batch, bboxes_batch, object_imgs_batch, projection_matricies_batch, instance_scene_batch = data
-    #print(f"{type(images_batch)=}, {type(bboxes_batch)=}, {type(object_imgs_batch)=}, {type(projection_matricies_batch)=}, {type(instance_scene_batch)=}")
-    #print(f"{len(images_batch)=}, {len(bboxes_batch)=}, {len(object_imgs_batch)=}, {len(projection_matricies_batch)=}, {len(instance_scene_batch)=}")
-    #print(f"{bboxes_batch=}")
-    #print(f"{bboxes_batch[0]=}")
-    #print(f"{bboxes_batch[0][0]=}")
+    object_imgs_batch, instance_scene_batch = next(iter(data_loader))
+    print(f"{len(object_imgs_batch)=}, {len(instance_scene_batch)=}")
 
-    for i, (images, bboxes, object_imgs, projection_matricies, instance_scene) in enumerate(zip(images_batch, bboxes_batch, object_imgs_batch, projection_matricies_batch, instance_scene_batch)):
-        for j, (image, bbox, object_img, P) in enumerate(zip(images, bboxes, object_imgs, projection_matricies)):
-            image = image.permute(1, 2, 0)
-            object_img = object_img.permute(1, 2, 0)
+    for i, (object_imgs_set, instance_scene_set) in enumerate(zip(object_imgs_batch, instance_scene_batch)):
+        print(f"{len(object_imgs_set)=}, {len(instance_scene_set)=}")
 
-            new_img = torch.zeros((image.shape[1] + object_img.shape[1] + 1, max(image.shape[1], object_img.shape[1]) + 1, 3))
-            new_img[:image.shape[0], :image.shape[1], :] = image
-            new_img[image.shape[0]:image.shape[0] + object_img.shape[0], :object_img.shape[1], :] = object_img
-
-            x1, y1, x2, y2 = bbox
-            bb_color = torch.tensor([1.0, 0.0, 0.0])
-            new_img[y1, x1:x2, :] = bb_color
-            new_img[y2, x1:x2, :] = bb_color
-            new_img[y1:y2, x1, :] = bb_color
-            new_img[y1:y2, x2, :] = bb_color
-
-            axes[i][j].imshow(new_img)
-            axes[i][j].axis('off')
-            #print(f"{i=}, {j=}")
+        for j, (object_img, instance_scene) in enumerate(zip(object_imgs_set, instance_scene_set)):
+            #print(f"{object_img.shape=}")
+            axes[j,i].imshow(object_img.permute(1,2,0))
+            axes[j,i].set_title(f"{instance_scene}\n {object_img.shape}", fontsize=5)
+            #print(dir(axes[i,j]))
+            axes[j,i].axis('off')
+    
+    plt.tight_layout()
     plt.show(block = False)
     plt.pause(1)
 
 
 
 if __name__ == "__main__":
-    bs = 2
+    bs = 4
+    set_size = 3
     label_dir = './MessyTableData/labels'
     os.makedirs('./MessyTableData/csvs', exist_ok=True)
     for label_file in os.listdir(label_dir):
         if label_file.endswith('.json'):
             file_path = os.path.join(label_dir, label_file)
-            dataset = MessyTableDataset(file_path, set_size=5)
+            dataset = MessyTableDataset(file_path, set_size=set_size, train=True)
             #dataset.debug()
-            data_loader = DataLoader(dataset, batch_size=bs, shuffle=True, num_workers=8)
-            for _ in (tqdm(data_loader)):
-                pass
+            data_loader = DataLoader(dataset, batch_size=bs, shuffle=True, num_workers=0, collate_fn=custom_collate)
+            #for _ in (tqdm(data_loader)):
+            #    pass
             display_batch(data_loader)
             #break
     #plt.show()

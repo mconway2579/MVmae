d3a1a0947bc73cf0c5c18c571111e53a52837a9b
mconway2579 <maxlconway@gmail.com>
2025-02-21 13:46:08 -0700

    ready for a training run

--- MaskedAutoEncoder.py
+++ MaskedAutoEncoder.py
@@ -11,9 +11,7 @@ class MaskedAutoEncoder(nn.Module):
         self.max_seq_length = max_seq_length+1 # set maximum sequence length as needed
 
         self.patch_size = patch_size
-        self.pixels_per_patch = patch_size * patch_size
-
-
+        self.pixels_per_patch = patch_size * patch_size *3
 
         # Set device
         if torch.cuda.is_available():
@@ -46,7 +44,7 @@ class MaskedAutoEncoder(nn.Module):
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
 
         # Final linear layer to project back to input dimension.
-        self.output_layer = nn.Linear(hidden_dim, 3)
+        self.output_layer = nn.Linear(hidden_dim, self.pixels_per_patch)
 
         self.to(self.device)
 
@@ -71,57 +69,58 @@ class MaskedAutoEncoder(nn.Module):
         x = x.transpose(2,1)
         print(f"after transpose {x.shape=}")
 
-        #mask = (torch.rand(x.size(0), 1, x.size(2), device=self.device) > mask_ratio).float()
-        #print(f"{mask.shape=}")
-        #x = x * mask
-        #print(f"masked x = {x.shape}")
+        mask = (torch.rand(x.size(0), x.size(1),1, device=self.device) > mask_ratio).float()
+        print(f"{mask.shape=}")
+        x = x * mask
+        masked_seq = x
+        print(f"masked x = {x.shape}")
 
-
-        
-
-        #x = self.embedding(x)
-        #print(f"after embedded {x.shape=}")
-        #x += self.positional_encoding[:x.size(1), :].unsqueeze(0)
-        #print(f"after PE {x.shape=}")
+        print(f"before embedded {x.shape=}")
+        x = self.embedding(x)
+        print(f"after embedded {x.shape=}")
+        x += self.positional_encoding[:x.size(1), :].unsqueeze(0)
+        print(f"after PE {x.shape=}")
 
         # Add class token
-        #batch_size = x.size(0)
-        #print(f"{batch_size=}")
-        #class_token = self.class_token.unsqueeze(0).unsqueeze(1).expand(batch_size, 1, -1)
-        #print(f"{class_token.shape=}")
+        batch_size = x.size(0)
+        print(f"{batch_size=}")
+        class_token = self.class_token.unsqueeze(0).unsqueeze(1).expand(batch_size, 1, -1)
+        print(f"{class_token.shape=}")
 
-        #x = torch.cat((class_token, x), dim=1)
-        #print(f"after CLS token {x.shape=}")
+        x = torch.cat((class_token, x), dim=1)
+        print(f"after CLS token {x.shape=}")
 
 
         # Apply the transformer encoder.
-        #memory = self.transformer_encoder(x) 
+        memory = self.transformer_encoder(x) 
 
         # Apply the transformer decoder.
-        #decoded = self.transformer_decoder(x, memory)
+        decoded = self.transformer_decoder(x, memory)
 
         # Extract the class token from the decoder output
-        #class_token_output = decoded[:, 0, :]
+        class_token_output = decoded[:, 0, :]
         #print(f"{class_token_output.shape=}")
         # Remove the sequence dimension and class token.
 
         # Project back to input dimension.
-        #reconstructed = self.output_layer(decoded[:, 1:, :])
+        reconstructed_sequence = self.output_layer(decoded[:, 1:, :])
         #print(f"{reconstructed.shape=}")
 
-        #reconstructed = reconstructed.reshape(dims[0], dims[1], dims[2], dims[3])
-        #reconstructed = reconstructed.permute(0,3,1,2)
-        #return reconstructed, mask, class_token_output
-        x = x.transpose(2,1)
+        
+        reconstructed_sequence = reconstructed_sequence.transpose(2,1)
         print(f"after transpose {x.shape=}")
-        x = F.fold(x, (H, W), kernel_size=self.patch_size, stride=self.patch_size)
-        return x, None, None
+        reconstructed_sequence = F.fold(reconstructed_sequence, (H, W), kernel_size=self.patch_size, stride=self.patch_size)
+
+        masked_img = masked_seq.transpose(2,1)
+        masked_img = F.fold(masked_img, (H, W), kernel_size=self.patch_size, stride=self.patch_size)
+
+        return reconstructed_sequence, masked_img, class_token_output
 if __name__ == "__main__":
     from MessyTableInterface import MessyTableDataset, display_batch
     from torch.utils.data import DataLoader
     import os
     import matplotlib.pyplot as plt
-    hidden_dim = 128
+    hidden_dim = 256
     batch_size = 4
     patch_size = 8
      
@@ -130,7 +129,7 @@ if __name__ == "__main__":
     dataset = MessyTableDataset(file_path, set_size=1, img_shape=(256,256))
     seq_length = dataset.image_shape[0] * dataset.image_shape[1]
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    display_batch(data_loader)
+    #display_batch(data_loader)
     data = next(iter(data_loader))
     _, _, object_imgs_batch, _, _ = data
     print(f"{len(object_imgs_batch)=}")
@@ -140,11 +139,22 @@ if __name__ == "__main__":
     model = MaskedAutoEncoder(hidden_dim, max_seq_length=seq_length, patch_size=patch_size)
     object_imgs_batch = object_imgs_batch.to(model.device)
     
-    output, mask, emb = model(object_imgs_batch)
+    output, masked_imgs, emb = model(object_imgs_batch)
+    print(f"{emb.shape=}")
 
-    print("Reconstructed Output:", output.shape)
-    display_img = torch.cat((object_imgs_batch, output), dim=1)
-    plt.imshow(display_img)
-    plt.show()
+    print(f"Reconstructed Output: {output.shape}")
+    print(f"masked image: {masked_imgs.shape}")
+    print(f"{output.shape=}, {object_imgs_batch.shape=}")
+
+    output = output.permute(0,2,3,1)
+    object_imgs_batch = object_imgs_batch.permute(0,2,3,1)
+    masked_imgs = masked_imgs.permute(0,2,3,1)
+
+    print(f"{output.shape=}, {object_imgs_batch.shape=}, {masked_imgs.shape=}")
+    for original, masked_img, reconstructed in zip(object_imgs_batch, masked_imgs, output):
+        print(f"{original.shape=}, {reconstructed.shape=}, {masked_imgs.shape=}")
+        display_img = torch.cat((original, masked_img,reconstructed), dim=1).detach().cpu().numpy()
+        plt.imshow(display_img)
+        plt.show()
     #print("Mask:", mask.shape)
     #print(f"{emb.shape=}")
